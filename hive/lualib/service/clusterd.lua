43683d3c4cd539ba1dbc4332a253798971e10c75
zkj <chimukuanglan@foxmail.com>
2021-05-12 15:14:25 +0800

    添加clusterd服务加载配置

--- hive/lualib/service/clusterd.lua
+++ hive/lualib/service/clusterd.lua
@@ -6,11 +6,17 @@ local string = string
 local pcall = pcall
 local ipairs = ipairs
 local setmetatable = setmetatable
+local io = io
+local load = load
+local type = type
+local pairs = pairs
 
 local command = {}
 
 local node_address = {}
 local node_sender = {}
+
+local config_name = "./clustername.lua"
 
 local connecting = {}
 
@@ -68,7 +74,41 @@ function command.sender(node)
     return node_channel[node]
 end
 
+local function loadconfig(tmp)
+    if tmp == nil then
+        tmp = {}
+        if config_name then
+            local f = assert(io.open(config_name))
+            local source = f:read "*a"
+            f:close()
+            assert(load(source, "@" .. config_name, "t", tmp))
+        end
+    end
+    local reload = {}
+    for name, address in pairs(tmp) do
+        assert(address == false or type(address) == "string")
+        if node_address[name] ~= address then
+            -- address changed
+            if node_sender[name] then
+                -- reset connection if node_sender[name] exist
+                node_channel[name] = nil
+                table.insert(reload, name)
+            end
+            node_address[name] = address
+        end
+    end
+    for _, name in ipairs(reload) do
+        -- open_channel would block
+        cell.fork(open_channel, node_channel, name)
+    end
+end
+
+function command.reload(config)
+    loadconfig(config)
+end
+
 cell.command(command)
 
 function cell.main()
+    loadconfig()
 end
