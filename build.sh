2654b17ef9b6fe3b90828bc33cd537c9931cdb82
root <root@localhost.localdomain>
2021-05-15 23:05:01 +0800

    修改linux执行文件输出路径¼

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rm -rf build hive/luaclib/* hive/core* hive/lua hive/lua.exe hive/lua.ilk hive/lua.pdb hive/luac hive/luac.exe hive/luac.ilk hive/luac.pdb
+rm -rf build hive/luaclib/* hive/core* hive/lua hive/lua.* hive/luac hive/luac.* hive/protoc*
 
 mkdir build
 
@@ -8,4 +8,4 @@ cd build
 
 cmake ..
 
-cmake --build . --config Debug --target all --clean-first
+cmake --build . --config Release --target all --clean-first
